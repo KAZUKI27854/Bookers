749c6b8ca38ff4e3a9de50bfa72425417d23a178
KAZUKI27854 <02kazuki16@gmail.com>
2021-02-12 12:01:56 +0000

    [Start] 3-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,25 @@
+class BooksController < ApplicationController
+  
+  def top
+  end
+  
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to new_book_path
+  end
+  
+  def index
+    @books = Book.all
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
