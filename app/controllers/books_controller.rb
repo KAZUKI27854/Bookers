f22b68ea4fe6a07f3a8c6e3520af2225ae310a5d
KAZUKI27854 <02kazuki16@gmail.com>
2021-02-13 06:03:17 +0000

    temporary saving

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,8 +13,29 @@ class BooksController < ApplicationController
     redirect_to new_book_path
   end
   
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+  
   def index
+    @book = Book.new
     @books = Book.all
+  end
+  
+  def show
+    @book = Book.find(params[:id])
   end
   
   private
